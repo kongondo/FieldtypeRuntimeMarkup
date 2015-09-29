3f1c4e4ba23c081a7d2e1a9a236330f0625ea6c4
kongondo <kongondo@gmail.com>
2015-09-29 23:32:29 +0100

    Some minor changes/cleanup

--- InputfieldRuntimeMarkup.js
+++ InputfieldRuntimeMarkup.js
@@ -1,13 +1,1 @@
 /*Use this file for scripts (js) for your renderend Markup in the page edit in admin, i.e. in the rendered InputfieldRuntimeMarkup*/
-
-	/**
-	 * Setup fancybox for page edits
-	 */
-	var h = $(window).height()-65;
-    var w = $(window).width() > 1150 ? 1150 : $(window).width()-100;
-	
-	$('.batcher_edit').fancybox({
-		type : 'iframe',
-		frameWidth : w,
-		frameHeight : h
-	});
